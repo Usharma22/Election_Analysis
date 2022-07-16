bd020d4ca7856da3c0f7a400361c88cad8e963cc
Your Name <you@example.com>
2022-07-16 16:33:38 -0700

    Adding the election analysis

--- PythonPoll.py
+++ PythonPoll.py
@@ -1,60 +1,39 @@
-# the data we need to retrieve
-#The total number of votes cast
-# A cmplete list of candidates who received votes
-#The percentage of votes each candidate won
-#the total number of votes each candidate won
-
-
-
-# indirect Path to load file
 import csv
 import os
-# Assign a variable for the file to load and the path.
-file_to_load = os.path.join("election_results.csv")
-# Open the election results and read the file.
-
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("Analysis", "election_analysis.txt")
-# Using the open() function with the "w" mode we will write data to the file.
-
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 # Initialize a total vote counter.
 total_votes = 0
-# Candidate options and candidate votes
+# Candidate options and candidate votes.
 candidate_options = []
 candidate_votes = {}
 # Track the winning candidate, vote count, and percentage.
-
-winning_candidate=""
-winning_count =0
-
-winning_percentage=0
-
-
-with open(file_to_load, "r") as election_data:
-     file_reader = csv.reader(election_data)
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
     # Read the header row.
-     headers = next(file_reader)
-
+    headers = next(file_reader)
     # Print each row in the CSV file.
-     for row in file_reader:
-          # Add to the total vote count.
-          total_votes += 1
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1
+        # Get the candidate name from each row.
+        candidate_name = row[2]
+        # If the candidate does not match any existing candidate, add the
+        # the candidate list.
+        if candidate_name not in candidate_options:
+            # Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+            # And begin tracking that candidate's voter count.
+            candidate_votes[candidate_name] = 0
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
 
-          # Print the candidate name from each row.
-          candidate_name = row[2]
-
-          if candidate_name not in candidate_options:
-
-               # Add the candidate name to the candidate list.
-               candidate_options.append(candidate_name)
-
-                    # 2. Begin tracking that candidate's vote count.
-               candidate_votes[candidate_name] = 0
-          # Add a vote to that candidate's count
-          candidate_votes[candidate_name] += 1
-
-     # Print the candidate vote dictionary.
-     #print(candidate_votes)  
 # Save the results to our text file.
 with open(file_to_save, "w") as txt_file:
     # After opening the file print the final vote count to the terminal.
